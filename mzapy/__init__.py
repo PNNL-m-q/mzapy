218474908d082639580fb369cc5f13ec88b08165
ross200 <dylan.ross@pnnl.gov>
2022-06-24 16:32:00 -0700

    added a function to view module for plotting TW CCS calibrations

--- mzapy/__init__.py
+++ mzapy/__init__.py
@@ -10,7 +10,7 @@ Joon-Yong Lee (junyoni@gmail.com)
 
 # mza_version.major_version.minor_version
 # mza_version is kept in lockstep with release of MZA format
-__version__ = '1.1.1.dylanhross_1'
+__version__ = '1.1.1.dylanhross_2'
 
 
 import queue
