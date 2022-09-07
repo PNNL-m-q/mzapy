6305dd059b5f282bf665468bb26517d1d1ac25cf
Ross, Dylan <dylan.ross@pnnl.gov>
2022-09-07 11:51:41 -0700

    added unit testing submodule

---
+++ mzapy/test/__init__.py
@@ -0,0 +1,7 @@
+"""
+mzapy/test/__init__.py
+
+Dylan Ross (dylan.ross@pnnl.gov)
+
+    sub-module for unit tests
+"""
---
+++ mzapy/test/__main__.py
@@ -0,0 +1,16 @@
+"""
+mzapy/test/__main__.py
+
+Dylan Ross (dylan.ross@pnnl.gov)
+
+    runs unit tests
+"""
+
+
+def _main():
+    """ main function for running all unit tests """
+    print('test main')
+
+
+if __name__ == '__main__':
+    _main()
