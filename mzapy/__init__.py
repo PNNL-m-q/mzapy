3b5e1b3242baec7ccc2e0e19ede9db184fdd5e9f
Ross, Dylan <dylan.ross@pnnl.gov>
2024-02-01 11:41:35 -0800

    expand element coverage in isotopes module

--- mzapy/__init__.py
+++ mzapy/__init__.py
@@ -10,7 +10,7 @@ Joon-Yong Lee (junyoni@gmail.com)
 
 # mza_version.major_version.minor_version
 # mza_version is kept in lockstep with release of MZA format
-__version__ = '1.7.1.dylanhross_4'
+__version__ = '1.7.1.dylanhross_5'
 
 
 import queue
