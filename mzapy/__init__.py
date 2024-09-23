6ce61f6a764f590bfd2af66e0286a553e3afc551
dylan.ross@pnnl.gov <ross200@we50940.pnl.gov>
2024-09-23 14:15:57 -0700

    processing data that is converted to MZA from mzML does not work because the spectra are not stored as TOF bins but instead as actual m/zs. This means there is no full_mz_array and the MZA wont even load. See if it is possible to remove the reliance on full_mz_array and spectra with TOF bins

--- mzapy/__init__.py
+++ mzapy/__init__.py
@@ -10,7 +10,7 @@ Joon-Yong Lee (junyoni@gmail.com)
 
 # mza_version.major_version.minor_version
 # mza_version is kept in lockstep with release of MZA format
-__version__ = '1.8.0'
+__version__ = '1.8.0.no_full_mz_array_0'
 
 
 import queue
